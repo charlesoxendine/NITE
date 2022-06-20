92ff8eebf97b7576fd02f37b835e6af147a52138
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2022-06-19 21:21:02 -0400

    added back info.plist

--- NITE/Application/SceneDelegate.swift
+++ NITE/Application/SceneDelegate.swift
@@ -16,7 +16,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let storyboard = UIStoryboard(name: "Auth", bundle: nil)
+            let newVC = storyboard.instantiateViewController(withIdentifier: "main")
+            window.rootViewController = newVC
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -57,6 +64,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
-
 }
-
