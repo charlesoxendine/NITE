fd4606e41bb028d69e757c5a2d01e767cdacf2bc
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2022-06-22 20:14:23 -0400

    up to date

--- NITE/Application/AppDelegate.swift
+++ NITE/Application/AppDelegate.swift
@@ -14,7 +14,9 @@ import RevenueCat
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
